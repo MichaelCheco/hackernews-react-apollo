88547d9d59c2eed7e8fe694679947947f0292e30
Michael Checo <checomichael2@gmail.com>
2018-12-09 00:32:40 -0500

    Added voting feature

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { AUTH_TOKEN } from './constants';
+import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers}) => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    return {
+        headers: {
+            ...headers,
+            authorizations: token ? `Bearer ${token}`: ''
+        }
+    }
+})
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 })
 
