91abc0adaf624719bfa83adde910aedbbd77d64f
Michael Checo <checomichael2@gmail.com>
2018-12-09 10:06:15 -0500

    store updates with new information without reloading

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,49 +1,64 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import Link from './Link';
-import gql from 'graphql-tag';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import Link from "./Link";
+import gql from "graphql-tag";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
-      feed {
-          links {
-              id
-              createdAt
-              url
-              description
-              postedBy {
-                  id
-                  name
-              }
-              votes {
-                  id
-                  user {
-                      id
-                  }
-              }
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
           }
+        }
       }
+    }
   }
-`
+`;
 
 class LinkList extends Component {
-    render() { 
-        return ( 
-            <Query query={FEED_QUERY}>
-                {({loading, error, data}) => {
-                    if(loading) return <div>Fetching</div>
-                    if(error) return <div>Error</div>
-                    const linksToRender = data.feed.links;
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
 
-                    return (
-                        <div>
-                            {linksToRender.map((link, index) => <Link key={link.id} index={index} link={link} />)}
-                        </div>
-                    )
-                }}
-            </Query>
-        )
-    }
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map((link, index) => (
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
 }
- 
+
 export default LinkList;
